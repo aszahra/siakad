77927a49ffbbdda5e04afeba1ce7b094ff36899f
AISYAH SITI ZAHRA <160479611+aszahra@users.noreply.github.com>
2024-07-07 19:36:02 +0700

    add sesi

--- app/Models/sesi.php
+++ app/Models/sesi.php
@@ -5,15 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Jurusan extends Model
+class sesi extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'kode_jurusan',
-        'jurusan'
+        'kode_sesi',
+        'sesi',
+        'pukul'
     ];
 
-    protected $table = 'jurusan';
+    protected $table = 'sesi';
 }
-                                    
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Jurusan') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('sesi.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Sesi') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
